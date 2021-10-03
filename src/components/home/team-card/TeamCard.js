004c57763256369a3da03d9fca5bb2b1f563fd5f
Сергей Герасимчук <s.gerasimchuk@avto.pro>
2021-10-03 12:24:05 +0300

    add prop-types

--- src/components/home/team-card/TeamCard.js
+++ src/components/home/team-card/TeamCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {useSelector} from "react-redux";
 
@@ -18,6 +19,10 @@ const TeamCard = ({team}) => {
             }} />
         </Link>
     );
+}
+
+TeamCard.propTypes = {
+    team: PropTypes.object
 }
 
 export default TeamCard;
